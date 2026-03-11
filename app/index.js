520fb5278a87519f59acfa44ba8513112a68e42d
rithish-126-kej <rithish.r@kej.co.in>
2026-03-11 18:08:05 +0530

    implement scholarship module to Apply

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import ScholarshipScreen from "./ScholarshipDetails";
+export default function Index() {
+  return (
+    <View
+      style={{flex: 1}}
+    >
+      <ScholarshipScreen />
+    </View>
+  );
+}
