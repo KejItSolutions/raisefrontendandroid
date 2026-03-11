90fb5ba630d72a79aa1ba32a21a5020bc704c9b7
Dileepreddy-017-kej <dileep.kumar@kej.co.in>
2026-03-11 18:21:20 +0530

    Added login and sign-in screens

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import LoginScreen from "./LoginScreen";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        
+      }}
+    >
+      <LoginScreen />
+    </View>
+  );
+}
