90563a5d47a5ed81d4a0cf1b21c2bdb6693cb447
Narendrakej <narandrababu@kej.co.in>
2026-03-16 11:33:04 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
 
-import StudentProfile from "./StudentProfile";
-
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <StudentProfile />
-
-=======
 import { View } from "react-native";
-
 import LoginScreen from "./LoginScreen";
-
-
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
      <LoginScreen />
-      
->>>>>>> origin/develop
     </View>
   );
 }
