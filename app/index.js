0917323885fd14189c830db730cfe19190881b90
Dileepreddy-017-kej <dileep.kumar@kej.co.in>
2026-03-17 14:46:09 +0530

    feat:Implement SportsAndAthletics

--- app/index.js
+++ app/index.js
@@ -1,13 +1,18 @@
 import { View } from "react-native";
-
 import LoginScreen from "./LoginScreen";
+
 
 
 export default function Index() {
   return (
-    <View style={{ flex: 1 }}>
-     <LoginScreen />
-      
+    <View
+      style={{
+        flex: 1,
+        
+      }}
+    >
+      <LoginScreen />
+     
     </View>
   );
 }
